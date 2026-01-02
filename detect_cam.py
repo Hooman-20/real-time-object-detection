ab2b28e883b5a3de297051fac332c9b467568e5f
Hooman Vatan Navaz <98411298+Hooman-20@users.noreply.github.com>
2026-01-02 10:52:30 -0800

    Update detect_cam.py

--- detect_cam.py
+++ detect_cam.py
@@ -6,6 +6,7 @@ from tritonclient.utils import InferenceServerException
 from coco_labels_91 import COCO_LABELS
 
 
+
 # Configuration
 TRITON_URL  = "localhost:8000"
 MODEL_NAME  = "ssd_mobilenet_v1"
